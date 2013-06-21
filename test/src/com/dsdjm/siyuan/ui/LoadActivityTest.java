6abfed9690e1508577511a970bff408ccea8334b
dsdjm <dsdjm2013@163.com>
2013-06-21 16:24:21 +0800

    add test project

---
+++ test/src/com/dsdjm/siyuan/ui/LoadActivityTest.java
@@ -0,0 +1,21 @@
+package com.dsdjm.siyuan;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.dsdjm.siyuan.ui.LoadActivityTest \
+ * com.dsdjm.siyuan.tests/android.test.InstrumentationTestRunner
+ */
+public class LoadActivityTest extends ActivityInstrumentationTestCase2<LoadActivity> {
+
+    public LoadActivityTest() {
+        super("com.dsdjm.siyuan", LoadActivity.class);
+    }
+
+}
